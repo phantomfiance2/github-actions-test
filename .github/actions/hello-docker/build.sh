383241c92c93afa13d97b2adb518c8e1b9179828
yuzheliu <yuzheliu@microsoft.com>
2021-07-21 22:16:59 -0700

    add more debug

--- .github/actions/hello-docker/build.sh
+++ .github/actions/hello-docker/build.sh
@@ -14,16 +14,32 @@ if [ ! -d "bin/" ]
 then
   mkdir bin/
 fi
+
+echo "just before download tar.gz, pwd:"
+pwd
+ls -la
+
 wget https://nginx.org/download/$nginxFullName.tar.gz -O $nginxFullName.tar.gz
 tar -xvf $nginxFullName.tar.gz
+
+echo "after download tar.gz, pwd:"
+pwd
+ls -la
+
 if [ ! -d "$nginxFullName/moduleSrc" ]
 then
   mkdir $nginxFullName/moduleSrc/
 fi
 cp -r $moduleFolder $nginxFullName/moduleSrc/
+
+echo "after copy ngx_mirror folder content, pwd:"
+pwd
+ls -la
+
 echo "just before running docker-compose"
 # docker-compose build --build-arg module_name=$moduleName --build-arg nginx_version=$nginxVersion 2>/dev/null
 cd ./.github/actions/hello-docker/.
 echo "current pwd:"
 pwd
+ls -la
 docker build -t docker-action --build-arg module_name=$moduleName --build-arg nginx_version=$nginxVersion . && docker run docker-action
