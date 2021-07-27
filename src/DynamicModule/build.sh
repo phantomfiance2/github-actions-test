d5154563ebb25039c32b6f14b37a851c0f24ab14
yuzheliu <yuzheliu@microsoft.com>
2021-07-27 09:54:53 -0700

    add linting step in workflow

--- src/DynamicModule/build.sh
+++ src/DynamicModule/build.sh
@@ -4,16 +4,37 @@ nginxFullName=nginx-$nginxVersion
 moduleFolder=$2;
 moduleName=$(basename "${moduleFolder}")
 # to be captured by TAP of Bats
-echo ${moduleName}
-echo ${nginxVersion}
+echo "$moduleName"
+echo "$nginxVersion"
 if [ ! -d "bin/" ]
 then
   mkdir bin/
 fi
-wget https://nginx.org/download/$nginxFullName.tar.gz -O $nginxFullName.tar.gz
-tar -xvf $nginxFullName.tar.gz
+wget https://nginx.org/download/"$nginxFullName".tar.gz -O "$nginxFullName".tar.gz
+tar -xvf "$nginxFullName".tar.gz
 if [ ! -d "$nginxFullName/moduleSrc" ]
 then
-  mkdir $nginxFullName/moduleSrc/
+  mkdir "$nginxFullName"/moduleSrc/
 fi
-cp -r ./$moduleFolder $nginxFullName/moduleSrc/
+cp -r ./"$moduleFolder" "$nginxFullName"/moduleSrc/
+
+
+# #!/bin/bash
+# nginxVersion=$1;
+# nginxFullName=nginx-$nginxVersion
+# moduleFolder=$2;
+# moduleName=$(basename "${moduleFolder}")
+# # to be captured by TAP of Bats
+# echo ${moduleName}
+# echo ${nginxVersion}
+# if [ ! -d "bin/" ]
+# then
+#   mkdir bin/
+# fi
+# wget https://nginx.org/download/$nginxFullName.tar.gz -O $nginxFullName.tar.gz
+# tar -xvf $nginxFullName.tar.gz
+# if [ ! -d "$nginxFullName/moduleSrc" ]
+# then
+#   mkdir $nginxFullName/moduleSrc/
+# fi
+# cp -r ./$moduleFolder $nginxFullName/moduleSrc/
